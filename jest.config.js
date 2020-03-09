9dd6d9b9f196da812baa120b2b351beca5c61664
lapinell <lapinell@gmail.com>
2020-03-08 22:30:06 -0500

    removed jest and lambda function

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': `<rootDir>/jest-preprocess.js`,
-  },
-  testPathIgnorePatterns: [`node_modules`, `.cache`],
-  transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
-};
