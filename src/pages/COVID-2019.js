c49037bab7ffeea3512a6c44627522f0cbeefee1
Hellrungj <johnjameshellrung@gmail.com>
2020-03-12 23:30:32 -0500

    Make an init COVID 2019 page with a title and some pargrapgh text

---
+++ src/pages/COVID-2019.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout'
+import { Card } from 'react-bootstrap'
+
+export default () => {
+  return (
+    <Layout>
+      <div>
+        <h2>About COVID 2019</h2>
+        <p>Non in qui ex cupidatat velit eu nisi. Elit ipsum sint quis cupidatat proident est. Amet ea eiusmod duis in non elit voluptate reprehenderit exercitation.</p>
+      </div>
+    </Layout>
+  );
+};
