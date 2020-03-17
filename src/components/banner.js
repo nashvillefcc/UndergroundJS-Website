a1ea3ca96cd95fe9bd41e94eb008f9d48408d6ff
Laura Pinell <lapinell@gmail.com>
2020-03-16 21:26:45 -0500

    adding banner example content

--- src/components/banner.js
+++ src/components/banner.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="container">
-            <h1>Banner</h1>
+        <div className="pageBanner">
+            <h1>Page Title</h1>
+            <p>Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with: “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</p>
         </div>
     )
 }
